e9fc1a0bdd37d99e9a047d91e50ddcec23c36ee0
Tafara <chipatotafara44@gmail.com>
2025-08-18 12:57:02 +0200

    Tafara l removed all the buttons that were at main activity to home activity

--- app/src/main/java/com/example/tlotlotau/HomeActivity.java
+++ app/src/main/java/com/example/tlotlotau/HomeActivity.java
@@ -2,22 +2,39 @@ package com.example.tlotlotau;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
 
 import com.example.tlotlotau.Auth.ProfileActivity;
+import com.example.tlotlotau.Documents.DocumentsActivity;
+import com.example.tlotlotau.Inventory.ManageProductsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.example.tlotlotau.Documents.DocumentsActivity;
+import com.example.tlotlotau.Inventory.ManageProductsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.tlotlotau.Auth.ProfileActivity;
+
 
 public class HomeActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     TextView tv_main_title;
     ImageButton btn_profile;
+    private Button btnSellProduct;
+    private Button btnManageProducts;
+    private Button btnViewDocuments;
+    private Button btnProfile;
+
 
 
     @Override
@@ -30,6 +47,9 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnViewDocuments = findViewById(R.id.btn_manage_documents);
+        btnSellProduct = findViewById(R.id.btn_sell_product);
+        btnManageProducts = findViewById(R.id.btn_manage_products);
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -37,6 +57,38 @@ public class HomeActivity extends AppCompatActivity {
         tv_main_title.setText("Welcome " + mAuth.getCurrentUser().getEmail().toString());
         btn_profile = findViewById(R.id.btn_profile);
         btn_profile.setOnClickListener(View -> startActivity(new Intent(HomeActivity.this, ProfileActivity.class)));
+        btnViewDocuments.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, DocumentsActivity.class)));
+        btnSellProduct.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, SellProductActivity.class)));
+        btnManageProducts.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, ManageProductsActivity.class)));
 
+        btnProfile.setOnClickListener(v -> {
+            if (mAuth.getCurrentUser() == null)
+            {
+                Toast.makeText(getApplicationContext(), "No User Found",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                startActivity(new Intent(this, HomeActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_settings) {
+                startActivity(new Intent(this, EditCompanyInfoActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_invoices) {
+                startActivity(new Intent(this, DocumentsActivity.class));
+            } else if (itemId == R.id.nav_estimates) {
+                startActivity(new Intent(this, DocumentsActivity.class));
+            }
+
+            return false;
+
+        });
     }
 }
--- app/src/main/java/com/example/tlotlotau/MainActivity.java
+++ app/src/main/java/com/example/tlotlotau/MainActivity.java
@@ -3,28 +3,23 @@ package com.example.tlotlotau;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tlotlotau.Auth.LoginActivity;
-import com.example.tlotlotau.Documents.DocumentsActivity;
-import com.example.tlotlotau.Inventory.ManageProductsActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import com.example.tlotlotau.Auth.RegisterActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.example.tlotlotau.Auth.ProfileActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSellProduct;
-    private Button btnManageProducts;
-    private Button btnViewDocuments;
 
     private Button btnRegister;
-    private Button btnProfile;
+
     private Button btnLogin;
 
     FirebaseAuth mAuth;
@@ -42,52 +37,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Bottom Navigation Logic
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-            if (itemId == R.id.nav_home) {
-                startActivity(new Intent(this, MainActivity.class));
-                return true;
-            } else if (itemId == R.id.nav_settings) {
-                startActivity(new Intent(this, EditCompanyInfoActivity.class));
-                return true;
-            } else if (itemId == R.id.nav_invoices) {
-                startActivity(new Intent(this, DocumentsActivity.class));
-            } else if (itemId == R.id.nav_estimates) {
-                startActivity(new Intent(this, DocumentsActivity.class));
-            }
 
-            return false;
-
-        });
 
         // Initialize buttons (make sure these IDs exist in your activity_main.xml)
-        btnViewDocuments = findViewById(R.id.btn_manage_documents);
-        btnSellProduct = findViewById(R.id.btn_sell_product);
-        btnManageProducts = findViewById(R.id.btn_manage_products);
+
         btnRegister = findViewById(R.id.btnRegister);
-        btnProfile = findViewById(R.id.btnProfile);
+
         btnLogin = findViewById(R.id.btnLogin);
 
 
         // Set click listeners for each button
-        btnViewDocuments.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, DocumentsActivity.class)));
-        btnSellProduct.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SellProductActivity.class)));
-        btnManageProducts.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ManageProductsActivity.class)));
+
         btnRegister.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
         btnLogin.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
         // Profile Button checks if user exists first
-        btnProfile.setOnClickListener(v -> {
-            if (mAuth.getCurrentUser() == null)
-                {
-                    Toast.makeText(getApplicationContext(), "No User Found",Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                    startActivity(intent);
-                }
-        });
+
         // Initialize the database
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
